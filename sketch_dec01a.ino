0ec6e46c16b50b6f20db7cd796451eb69cb2c4dd
leeparkhae <44611453+leeparkhae@users.noreply.github.com>
2018-12-08 17:00:09 +0100

    Add files via upload

---
+++ sketch_dec01a.ino
@@ -0,0 +1,21 @@
+ int ENA=5;
+ int in1=6;
+ int in2=7;
+ void setup() {
+ pinMode(ENA, OUTPUT);
+ pinMode(in1, OUTPUT);
+ pinMode(in2, OUTPUT);
+}
+
+void loop() {
+ digitalWrite(ENA, HIGH);
+ digitalWrite(in1, HIGH);
+ digitalWrite(in2, LOW);
+ delay(1000);
+ digitalWrite(ENA, LOW);
+ delay(1000);
+ analogWrite(ENA, 120);
+ delay(1000);
+ digitalWrite(ENA, LOW);
+ delay(1000);
+}
